a7c3c9ed816be5609513a48dca3ddfc976f6ba20
Sergey Bendas <seregabendas@gmail.com>
2018-06-22 12:39:21 +0300

    Rename variables

--- src/main/java/com/bendas/wholesquarecount/Solution.java
+++ src/main/java/com/bendas/wholesquarecount/Solution.java
@@ -2,16 +2,16 @@ package com.bendas.wholesquarecount;
 
 public class Solution {
 
-  public int solution(int val1, int val2) {
-    if (val1 < 0 || val2 < 0) {
+  public int solution(int A, int B) {
+    if (A < 0 || B < 0) {
       throw new RuntimeException("Wrong value");
     }
-    if (val1 > val2) {
+    if (A > B) {
       throw new RuntimeException("First value is bigger then second one");
     }
-    double val2sqrt = Math.sqrt(val2);
-    double val1sqrt = Math.sqrt(val1);
-    int result = (int) Math.floor(val2sqrt) - (int) Math.ceil(val1sqrt) + 1;
+    double bSqrt = Math.sqrt(B);
+    double aSqrt = Math.sqrt(A);
+    int result = (int) Math.floor(bSqrt) - (int) Math.ceil(aSqrt) + 1;
     return result;
   }
 }
